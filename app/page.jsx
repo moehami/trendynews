d0705d62589d70475f9f6611d04fe2d05fde1299
moehami <m.aljaaly@yahoo.co.uk>
2024-08-18 11:03:44 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,24 @@
 import Link from 'next/link';
-import { Card } from 'components/card';
-import { RandomQuote } from 'components/random-quote';
-import { Markdown } from 'components/markdown';
-import { ContextAlert } from 'components/context-alert';
-import { getNetlifyContext } from 'utils';
+//import { Card } from 'components/card';
+//import { RandomQuote } from 'components/random-quote';
+//import { Markdown } from 'components/markdown';
+//import { ContextAlert } from 'components/context-alert';
+//import { getNetlifyContext } from 'utils';
 
-const cards = [
+//const cards = [
     //{ text: 'Hello', linkText: 'someLink', href: '/' }
 ];
 
-const contextExplainer = `
+//const contextExplainer = `
 The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
 ([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
 `;
 
-const preDynamicContentExplainer = `
+//const preDynamicContentExplainer = `
 The card content below is fetched by the client-side from \`/quotes/random\` (see file \`app/quotes/random/route.js\`) with a different quote shown on each page load:
 `;
 
-const postDynamicContentExplainer = `
+//const postDynamicContentExplainer = `
 On Netlify, Next.js Route Handlers are automatically deployed as [Serverless Functions](https://docs.netlify.com/functions/overview/).
 Alternatively, you can add Serverless Functions to any site regardless of framework, with acccess to the [full context data](https://docs.netlify.com/functions/api/).
 
