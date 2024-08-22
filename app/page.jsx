a99aef3e34413bd4676fe9f0deab0915c4528152
moehami <m.aljaaly@yahoo.co.uk>
2024-08-22 16:22:50 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -30,7 +30,7 @@ const ctx = getNetlifyContext();
 export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
-            <section className="flex flex-col items-start gap-3 sm:gap-4">
+
                 <ContextAlert />
                 <h1 className="mb-0">oyster sauce vs fish sauce </h1>
                 <p className="text-lg">Get started with Next.js and Netlify in seconds.</p>
