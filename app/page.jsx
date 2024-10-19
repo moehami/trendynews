c83d8a866d368566a9f0ff8554f8b610a0282843
moehami <m.aljaaly@yahoo.co.uk>
2024-10-19 09:32:37 +0300

    Update page.jsx
    Fix something

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,24 @@
 import Link from 'next/link';
-//import { Card } from 'components/card';
-//import { RandomQuote } from 'components/random-quote';
-//import { Markdown } from 'components/markdown';
-//import { ContextAlert } from 'components/context-alert';
-//import { getNetlifyContext } from 'utils';
+import { Card } from 'components/card';
+import { RandomQuote } from 'components/random-quote';
+import { Markdown } from 'components/markdown';
+import { ContextAlert } from 'components/context-alert';
+import { getNetlifyContext } from 'utils';
 
-//const cards = [
+const cards = [
     //{ text: 'Hello', linkText: 'someLink', href: '/' }
-//];
+];
 
-//const contextExplainer = `
+const contextExplainer = `
 The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
 ([docs](https://docs.netlify.com/configure-builds/environment-variables/#build-metadata)):
 `;
 
-//const preDynamicContentExplainer = `
+const preDynamicContentExplainer = `
 The card content below is fetched by the client-side from \`/quotes/random\` (see file \`app/quotes/random/route.js\`) with a different quote shown on each page load:
 `;
 
-//const postDynamicContentExplainer = `
+const postDynamicContentExplainer = `
 On Netlify, Next.js Route Handlers are automatically deployed as [Serverless Functions](https://docs.netlify.com/functions/overview/).
 Alternatively, you can add Serverless Functions to any site regardless of framework, with acccess to the [full context data](https://docs.netlify.com/functions/api/).
 
@@ -30,7 +30,7 @@ const ctx = getNetlifyContext();
 export default function Page() {
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
-
+            <section className="flex flex-col items-start gap-3 sm:gap-4">
                 <ContextAlert />
                 <h1 className="mb-0">oyster sauce vs fish sauce </h1>
                 <p className="text-lg">Get started with Next.js and Netlify in seconds.</p>
